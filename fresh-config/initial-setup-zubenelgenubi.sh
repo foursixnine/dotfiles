2dc53ae64f16f7623eef2ec6a270f975f1bcf2aa
Santiago Zarate <santiago@zarate.net.ve>
2016-08-15 01:30:51 -0500

    Fixing local::lib installation

--- fresh-config/initial-setup-zubenelgenubi.sh
+++ fresh-config/initial-setup-zubenelgenubi.sh
@@ -18,8 +18,13 @@ git clone git@github.com:foursixnine/mars-configurations.git
 
 wget http://search.cpan.org/CPAN/authors/id/H/HA/HAARG/local-lib-2.000019.tar.gz
 tar -xvzf local-lib-2.000019.tar.gz -C /tmp
-( cd /tmp/local-lib-2.000019 && perl Makefile.PL --bootstrap )
-echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >> ~/.bashrc
+(
+  cd /tmp/local-lib-2.000019
+  perl Makefile.PL --bootstrap
+  make test
+  make install
+  echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >>~/.bashrc
+)
 
 source ~/.bashrc 
 curl -L https://raw.githubusercontent.com/miyagawa/cpanminus/master/cpanm | perl - App::cpanminus
