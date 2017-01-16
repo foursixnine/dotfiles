255cead3ede44b2a253e656fe2ba1452c6697d0b
Santiago Zarate <santiago@zarate.net.ve>
2017-01-16 18:40:33 +0100

    Add openQA workspace specific
    With openqa-workspace.sh (which should be in the $PATH, we get a
    working setup which will result in a tmux session with 5 windows
    started in all the directories used by an openQA backend developer.
    
    Also variable $OPENQA will be exported, and three functions will be
    available to the user:
    
     # These are meant to be started from within the openQA git repository
      * start_openqa - starts openqa's gru, websockets, scheduler and webui
      * restart_openqa - kills all jobs and starts openqa
      * start_openqa_worker - starts openqa worker 1 with verbose and git's
         isotovideo

---
+++ openqa-workspace.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export OPENQA="$HOME/Projects/suse.com/github.com/os-autoinst"
+
+tmux new -s openqa-workspace -d
+#tmux new-window -topenqa-workspace:0 -n "root"
+tmux new-window -topenqa-workspace:1 -n "os-autoinst"
+tmux new-window -topenqa-workspace:2 -n "openQA WebUI"
+tmux new-window -topenqa-workspace:3 -n "openQA tests"
+tmux new-window -topenqa-workspace:4 -n "openQA Monitoring"
+tmux new-window -topenqa-workspace:5 -n "Workspace"
+
+tmux send-keys -topenqa-workspace:0 "sleep 10 & sudo su -;" C-m
+tmux send-keys -topenqa-workspace:1 "cd ${OPENQA}/os-autoinst; clear" C-m
+tmux send-keys -topenqa-workspace:2 "cd ${OPENQA}/openQA; clear" C-m
+tmux send-keys -topenqa-workspace:3 "cd ${OPENQA}/os-autoinst-distri-opensuse; clear" C-m
+tmux send-keys -topenqa-workspace:4 "cd ${OPENQA}/openQA; clear" C-m
+tmux send-keys -topenqa-workspace:5 "cd ${OPENQA}/; clear" C-m
+
+tmux select-window -topenqa-workspace:0
+tmux attach-session -topenqa-workspace
