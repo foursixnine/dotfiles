f43a4b744ce13c1bdf126a7703d3731c55b58b4f
Santiago Zarate <santiago@zarate.co>
2026-08-07 08:33:20 +0200

    Add nvim configuration

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,24 @@
+--- /** // options I like*/
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+-- Maybe we dont need to change this
+-- vim.g.mapleader=' '
+vim.wo.colorcolumn = "80,120"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("config.lazy")
---
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add tsx and treesitter
+    vim.list_extend(opts.ensure_installed, {
+      "rust",
+      "perl",
+      "markdown",
+      "python",
+    })
+  end,
+}
